6b7ed2df7f35c0567b492e93740fdeb0f1b1aac3
SaMuCoder <samucybert@gmail.com>
2023-01-04 14:16:44 -0100

    Changed class name Mensagem to Noticia

--- projetowservices/daw/models.py
+++ projetowservices/daw/models.py
@@ -3,7 +3,13 @@ from django.db import models
 # Create your models here.
 
 
-class Mensagem(models.Model):
+class Noticia(models.Model):
     titulo = models.CharField(max_length=20)
+    descricao = models.CharField(max_length=255)
+    data = models.DateTimeField(auto_now_add=True)
+
+
+class Comentario(models.Model):
+    noticia = models.ForeignKey(Noticia, on_delete=models.CASCADE)
     descricao = models.CharField(max_length=255)
     data = models.DateTimeField(auto_now_add=True)
