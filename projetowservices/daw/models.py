2ae79d96f7e856637a9053124699ff1273d7514c
SaMuCoder <samucybert@gmail.com>
2023-01-04 14:13:58 -0100

    Added classes

--- projetowservices/daw/models.py
+++ projetowservices/daw/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Mensagem(models.Model):
+    titulo = models.CharField(max_length=20)
+    descricao = models.CharField(max_length=255)
+    data = models.DateTimeField(auto_now_add=True)
