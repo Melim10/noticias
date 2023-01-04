404e400f0d746c38cd2801caccecddaa4359f9ef
Marcelo Melo <melim10@Marcelos-MacBook-Pro.local>
2023-01-04 14:36:52 -0100

    Url

--- projetowservices/projetowservices/urls.py
+++ projetowservices/projetowservices/urls.py
@@ -15,7 +15,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from daw import views
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('noticia/', admin.site.urls),
 ]
